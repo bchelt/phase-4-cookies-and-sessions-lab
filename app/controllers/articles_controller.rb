c6eb25b87fdb89c1ed655167947422eef87e63e4
Brandon Helt <brandon.helt@yahoo.com>
2022-09-10 22:17:59 -0700

    Completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :set_session
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -7,8 +8,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if session[:page_views] < 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render :json => {error: "Maximum pageview limit reached"}, :status => 401
+    end
+    session[:page_views] += 1
   end
 
   private
@@ -17,4 +23,7 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  def set_session
+    session[:page_views] ||= 0
+  end
 end
